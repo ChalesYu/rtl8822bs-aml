4dfa50ab6150aa51a6e03bbcd479c8d2e68bfa2b
zy <574249312@qq.com>
2019-01-21 12:23:50 +0800

    add stop_bt.sh

--- bluetooth/start_bt.sh
+++ bluetooth/start_bt.sh
@@ -9,8 +9,24 @@ echo "(Re)start Bluetooth device ..."
 sleep 1
 echo
 echo "Power off BT device now ..."
-rmmod 8822BS_BT_EN
 
+have_bt_en=`lsmod | grep 8822BS_BT_EN`
+if [ -z "$have_bt_en" ];
+then
+ echo "[ not find 8822BS_BT_EN ]"
+else
+ echo "find 8822BS_BT_EN , do rmmod "
+ rmmod 8822BS_BT_EN
+fi
+
+hciattach_pid=`ps -aux | grep hciattach | grep -v grep | awk '{print $2}'`
+if [ -z "$hciattach_pid" ];
+then
+ echo "[ not find hciattach pid ]"
+else
+ echo "find result: $hciattach_pid "
+ kill  $hciattach_pid
+fi
 
 echo "starting BT device now ..."
 sleep 1
